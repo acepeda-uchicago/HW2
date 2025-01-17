a2c76aef424886eaa65ded8e196f2f3891d46028
Alec Cepeda <aleccepeda@Alecs-MacBook-Air.local>
2025-01-16 21:04:03 -0600

    cleaning up

--- HW2Q1/HW2Q1.cpp
+++ HW2Q1/HW2Q1.cpp
@@ -4,27 +4,31 @@
 #include <cmath>
 
 int main(){
-    std::vector<double> example = {1.0, 2.0, 3.0};
+    // create a vector of doubles and a copy since transform (Part1/2)
+    // will change the value of example1
+    std::vector<double> example1 = {1.0, 2.0, 3.0};
+    std::vector<double> example2 = example1;
 
     // Part 1
-    // std::transform takes 3 main arguments:
-        // The beginning and end iterators of the range to transform (here, the entire numbers vector).
-        // The beginning iterator of the output range (again, the beginning of numbers to overwrite the original values).
-        // A unary operation (here, a lambda function that squares its input).
-    std::transform(example.begin(), example.end(), example.begin(), 
+    std::transform(example1.begin(), example1.end(), example1.begin(), 
                 [](double elem) { return elem * elem; });
     std::cout << "Part 1:" << std::endl;
-    for (double elem : example) {
+    for (double elem : example1) {
         std::cout << elem << " ";
     }
     std::cout << std::endl;
 
     // Part 2
-    double distance = sqrt(std::accumulate(example.begin(), example.end(), 0));
-    std::cout << "Part 2:" << std::endl;
-    std::cout << distance << std::endl;
+    double distance_acc = std::sqrt(std::accumulate(example1.begin(), example1.end(), 0));
+    std::cout << "Part 2:" << std::endl << distance_acc << std::endl;
 
     // Part 3
+    double distance_innerprod = std::sqrt(std::inner_product(example2.begin(), example2.end(), example2.begin(), 0.0));
+    std::cout << "Part 3:" << std::endl << distance_innerprod << std::endl;
+
+    // Part 4
+    double distance_euc = std::sqrt(std::accumulate(example2.begin(), example2.end(), 0, [](double a, double b) { return a +  b * b; }));
+    std::cout << "Part 4" << std::endl << distance_euc << std::endl;
 
     return 0;
 }
